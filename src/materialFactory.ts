503060291125abc02fd4f5589af4f3a9f7a98fd8
Yusuke Kawamoto <novogrammer@gmail.com>
2025-05-07 01:57:47 +0900

    TSLを色々ためした。bumpMapはTextureNodeを前提としているので諦めた。

--- src/materialFactory.ts
+++ src/materialFactory.ts
@@ -1,16 +1,22 @@
 import * as THREE from 'three/webgpu';
-import { vec3, sin, positionWorld } from 'three/tsl';
+import { positionLocal, mx_noise_float,vec3,fract,smoothstep } from 'three/tsl';
 
 /**
  * 動的に色が変化するマテリアルを生成するファクトリメソッド
  * @returns THREE.MeshStandardNodeMaterial
  */
 export function createDynamicMaterial(): THREE.MeshStandardNodeMaterial {
-  return new THREE.MeshStandardNodeMaterial({
-    colorNode: vec3(
-      sin(positionWorld.x.mul(0.5)).mul(0.5).add(0.5), // 赤成分
-      sin(positionWorld.y.mul(0.5)).mul(0.5).add(0.5), // 緑成分
-      sin(positionWorld.z.mul(0.5)).mul(0.5).add(0.5)  // 青成分
-    )
+  const contourBase=fract(mx_noise_float(positionLocal.xyz).mul(0.5).add(0.5).mul(50));
+  const contourA=smoothstep(0,0.1,contourBase);
+  const contourB=smoothstep(0.2,0.1,contourBase);
+  const contour=contourA.mul(contourB).negate();;
+  const myColor=vec3(1,0.5,0.35);
+
+  const material= new THREE.MeshStandardNodeMaterial({
+    colorNode: myColor.mul(contour.mul(0.2).add(0.8)),
+    roughness:0.4,
+    metalness:0.1,
   });
+
+  return material;
 }
