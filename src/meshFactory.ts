531642da31895bce48e98d54949d34383280d771
Yusuke Kawamoto <novogrammer@gmail.com>
2025-05-06 19:13:08 +0900

    不要なメソッドを削除

--- src/meshFactory.ts
+++ src/meshFactory.ts
@@ -2,26 +2,7 @@ import * as THREE from 'three/webgpu';
 import { vec3, sin, time } from 'three/tsl';  
 import { TeapotGeometry } from 'three/addons/geometries/TeapotGeometry.js';
   
-export function createCube(): THREE.Mesh {  
-  const geometry = new THREE.BoxGeometry();  
-    
-  // 時間経過で色が変化するマテリアル  
-  const material = new THREE.MeshStandardNodeMaterial({  
-    // 時間に基づいて色を動的に変更  
-    colorNode: vec3(  
-      sin(time.mul(0.5)).mul(0.5).add(0.5), // 赤成分  
-      sin(time.mul(0.3)).mul(0.5).add(0.5), // 緑成分  
-      sin(time.mul(0.2)).mul(0.5).add(0.5)  // 青成分  
-    )  
-  });  
-  
-  return new THREE.Mesh(geometry, material);  
-}
 
-/**
- * ティーポットを生成するファクトリメソッド
- * @returns THREE.Mesh
- */
 export function createTeapot(): THREE.Mesh {
   const geometry = new TeapotGeometry(1, 10); // サイズ1、分割数10
 
